bf2764562117ad6fd7193ca7eac18916226269e7
RickMar0 <dj95.maro@gmail.com>
2025-01-24 15:42:34 +0100

    remove cart item from the page after deletion

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,8 @@ cart.forEach((item) => {
     }
   })
   cartSummaryHTML += `
-<div class="cart-item-container">
+<div class="cart-item-container 
+  js-cart-item-container-${matchingProduct.id}">
   <div class="delivery-date">
     Delivery date: Tuesday, June 21
   </div>
@@ -99,5 +100,6 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
+    document.querySelector(`.js-cart-item-container-${productId}`).remove();
   })
 })
